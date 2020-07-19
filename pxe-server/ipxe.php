00d71628a023bca172f9e5eefb26567e0ac164e7
JanRK <jk@itmatic.dk>
2020-07-20 01:30:04 +0200

    Try this

--- pxe-server/ipxe.php
+++ pxe-server/ipxe.php
@@ -1,5 +1,13 @@
 #!ipxe
 
-kernel vmlinuz-3.16.0-rc4 bootfile=http://boot.ipxe.org/demo/boot.php fastboot initrd=initrd.img
-initrd initrd.img
-boot
+
+
+:macrium-winpe
+# sanboot --no-describe ${boot-url}/macrium-5.0/macrium-5.0-rescue-winpe-3.1.iso || goto failed
+sanboot --no-describe https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-10.4.0-amd64-netinst.iso || goto failed
+
+goto start
+
+# kernel vmlinuz-3.16.0-rc4 bootfile=http://boot.ipxe.org/demo/boot.php fastboot initrd=initrd.img
+# initrd initrd.img
+# boot
