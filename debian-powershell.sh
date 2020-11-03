bf7a97e0a89ce2545dc60cf9cd02482696eacfb5
JanRK <jk@itmatic.dk>
2020-11-03 19:36:28 +0100

    Add powershell installer script

---
+++ debian-powershell.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install -y --no-install-recommends gnupg wget ca-certificates
+wget --directory-prefix=/usr/share/keyrings https://packages.microsoft.com/keys/microsoft.asc
+gpg --dearmor --yes /usr/share/keyrings/microsoft.asc
+. /etc/os-release
+echo "deb [signed-by=/usr/share/keyrings/microsoft.asc.gpg] https://packages.microsoft.com/debian/$VERSION_ID/prod $VERSION_CODENAME main" > /etc/apt/sources.list.d/microsoft.list
+apt-get update
+apt-get install -y --no-install-recommends powershell
